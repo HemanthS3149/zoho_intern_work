34ee7b08543143a638f4e5963213e96c08e97366
HemanthS3149 <158204310+HemanthS3149@users.noreply.github.com>
2024-07-15 18:12:38 +0530

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -6,9 +6,9 @@ def fetch_data(url):
     try:
         response=requests.get(url)
         response.raise_for_status() #we ensure we process only successfull operations from the server
-        return response.json()
+        return response.json()#for human readability, converts HTTP response into a python dictionary or lis
     
-    except requests.exceptions.HTTPError as http_err:
+    except requests.exceptions.HTTPError as http_err: #http server fails to get processes
         print(f"HTTP error occurred:{http_err}")
     except Exception as err:
         print(f"Other error occurred: {err}")
